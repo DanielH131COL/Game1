70f52c672b4cf59e854baa3fd3cad080ab477c3e
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-06-19 18:06:13 -0500

    Added Configuracion

---
+++ src/main/java/github/com/danielh131col/utils/Configuracion.java
@@ -0,0 +1,6 @@
+package github.com.danielh131col.utils;
+
+public class Configuracion {
+    public static int velocidad = 700;      // milisegundos por movimiento (default)
+    public static int fallosMaximos = 50;   // fallos permitidos antes de Game Over
+}
